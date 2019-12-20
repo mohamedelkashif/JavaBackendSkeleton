b7c6905da23557e210d7d4d5ac1617a2ee9ecab5
Mohamed <mohamedmhassann@gmail.com>
2019-12-20 19:04:29 +0200

    Initialization of project

---
+++ src/main/java/com/test/skeletonProject/SkeletonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.skeletonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/skeletonProject/SkeletonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.skeletonProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
